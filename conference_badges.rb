831ce960adea049da8711d6934dcc2244310d71e
Michelle M <michellemoon236@gmail.com>
2019-03-18 23:43:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker (name)
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_creator (array)
+  badge_maker (array)
+end
+
+def assign_rooms (speaker)
+  "Hello, #{speaker}! You'll be assigned to room "
+end
+
+def printer
+  puts batch_badge_creator
+  puts assign_rooms
+end
+
