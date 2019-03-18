605e09f66fe1d116302e4a37527737b732f97ab4
Michelle M <michellemoon236@gmail.com>
2019-03-18 23:57:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,16 +2,17 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator (array)
-  array.collect do |name|
+def batch_badge_creator (array1)
+  array1.collect do |name|
     badge_maker (name)
 end
 end
 
 
 
-def assign_rooms (speaker)
-  "Hello, #{speaker}! You'll be assigned to room "
+def assign_rooms (array2)
+  array2.collect do |speaker|
+    "Hello, #{speaker}! You'll be assigned to room "
 end
 
 def printer
