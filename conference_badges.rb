52a1d5cdb343169b9733c0e2e2fd2bf0207366c7
Michelle M <michellemoon236@gmail.com>
2019-03-18 23:56:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,11 @@ def badge_maker (name)
 end
 
 def batch_badge_creator (array)
-  batch_array = []
-  array.each do |name|
-    batch_array << badge_maker(name)
-  end
+  array.collect do |name|
+    badge_maker (name)
 end
+end
+
 
 
 def assign_rooms (speaker)
