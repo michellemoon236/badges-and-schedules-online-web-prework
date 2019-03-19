2a9be6dbd80262b9e2e39bdeb1be43195340ec1d
Michelle M <michellemoon236@gmail.com>
2019-03-19 00:06:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,7 @@ def assign_rooms (array2)
   array2.each_with_index do |speaker, index|
     speaker_rooms << "Hello, #{speaker}! You'll be assigned to room #{index +1}!"
 end
+speaker_rooms
 end
 
 def printer
