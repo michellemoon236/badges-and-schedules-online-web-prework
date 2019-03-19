9e99e47738213eaaa8b9dfd40a5a5cec15b440d1
Michelle M <michellemoon236@gmail.com>
2019-03-19 00:28:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ speaker_rooms
 end
 
 def printer (attendees)
-   batch_badge_creator (attendees)
-   assign_rooms (attendees)
+   puts batch_badge_creator (attendees)
+   puts assign_rooms (attendees)
 end
 
