94b0c03425fabe527a55fbbbcd9becb423fd76e7
Michelle M <michellemoon236@gmail.com>
2019-03-19 00:15:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,12 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator (array1)
-  array1.collect do |name|
-    badge_maker (name)
-end
+def batch_badge_creator (array)
+  batch_array = [ ]
+  array.each do |name|
+    batch_array << badge_maker (name)
+  end
+  batch_array
 end
 
 
